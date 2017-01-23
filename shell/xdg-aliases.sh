30d138bd88b584f6b91433e78d5a41ce737d947c
Steve Buzonas <steve@fancyguy.com>
2017-01-23 16:15:07 -0500

    update xdg helpers

--- shell/xdg-aliases.sh
+++ shell/xdg-aliases.sh
@@ -2,6 +2,7 @@
 
 # Alias supported allications
 alias ag="ag -p $XDG_CONFIG_HOME/ag/agignore"
+alias emacs="emacs -q --load $XDG_CONFIG_HOME/emacs/init"
 alias scp="scp -F $XDG_CONFIG_HOME/ssh/config"
 alias ssh="ssh -F $XDG_CONFIG_HOME/ssh/config"
 alias tmux="tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf"
--- shell/xdg-env.sh
+++ shell/xdg-env.sh
@@ -12,11 +12,13 @@ export XDG_LIB_HOME="$SYSTEM_LOCAL_HOME/lib"
 # Environment variable supported applications (CONFIG_HOME)
 export AWS_CONFIG_FILE="$XDG_CONFIG_HOME/aws/config"
 export AWS_SHARED_CREDENTIALS_FILE="$XDG_CONFIG_HOME/aws/credentials"
+export GEMRC="$XDG_CONFIG_HOME/gem/gemrc"
 export GIT_SSH_COMMAND="ssh -F $XDG_CONFIG_HOME/ssh/config "$@" 2>/dev/null"
 export GNUPGHOME="$XDG_CONFIG_HOME/gnupg"
 export HTOPRC="$XDG_CONFIG_HOME/htop/htoprc"
 export HTTPIE_CONFIG_DIR="$XDG_CONFIG_HOME/httpie"
 export INPUTRC="$XDG_CONFIG_HOME/readline/inputrc"
+export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/config"
 export TERMINFO="$XDG_CONFIG_HOME/terminfo"
 export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
 
@@ -24,7 +26,7 @@ export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
 export LESSHISTFILE="$XDG_DATA_HOME/less/history"
 export MYSQL_HISTFILE="$XDG_DATA_HOME/mysql/history"
 export PSQL_HISTORY="$XDG_DATA_HOME/psdl/history"
-export VAGRANT_DOTFILE_PATH="$XDG_DATA_HOME/vagrant"
+#export VAGRANT_DOTFILE_PATH="$XDG_DATA_HOME/vagrant"
 export VAGRANT_HOME="$XDG_DATA_HOME/vagrant"
 
 # Environment variable supported applications (CACHE_HOME)

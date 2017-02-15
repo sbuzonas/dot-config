25b069d2e48bff056c9d4cd71221e5bdfea3164c
Steve Buzonas <steve@fancyguy.com>
2017-02-15 12:25:44 -0500

    move ssh config

--- shell/xdg-aliases.sh
+++ shell/xdg-aliases.sh
@@ -3,6 +3,6 @@
 # Alias supported allications
 alias ag="ag -p $XDG_CONFIG_HOME/ag/agignore"
 alias emacs="emacs -q --load $XDG_CONFIG_HOME/emacs/init"
-alias scp="scp -F $XDG_CONFIG_HOME/ssh/config"
-alias ssh="ssh -F $XDG_CONFIG_HOME/ssh/config"
+alias scp="scp -F $XDG_DATA_HOME/ssh/config"
+alias ssh="ssh -F $XDG_DATA_HOME/ssh/config"
 alias tmux="tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf"

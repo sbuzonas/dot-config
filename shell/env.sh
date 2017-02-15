37087098a718f64ef1dcee6713adf39328419494
Steve Buzonas <steve@fancyguy.com>
2017-02-15 13:14:51 -0500

    add coreutils paths

--- shell/env.sh
+++ shell/env.sh
@@ -3,8 +3,9 @@
 source "$XDG_CONFIG_HOME/shell/xdg-env.sh"
 
 [ -d /usr/local/opt/coreutils ] &&
-    export PATH="/usr/local/opt/coreutile/libexec/gnubin:$PATH" &&
-    export MANPATH="/usr/local/opt/coreutile/libexec/gnuman:$MANPATH"
+    export PATH="/usr/local/opt/coreutils/bin:$PATH" &&
+    export MANPATH="/usr/local/opt/coreutils/share/man:$MANPATH" &&
+    export INFOPATH="/usr/local/opt/coreutils/share/info:$INFOPATH"
 
 export PATH="$HOME/.local/bin:$HOME/.local/share/node/bin:$PATH"
 

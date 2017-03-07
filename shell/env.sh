eb4a6c2b7aac170c8a623f91157838e2f41ddc11
Steve Buzonas <steve@fancyguy.com>
2017-03-06 20:00:36 -0500

    add paths for latex

--- shell/env.sh
+++ shell/env.sh
@@ -7,6 +7,9 @@ source "$XDG_CONFIG_HOME/shell/xdg-env.sh"
     export MANPATH="/usr/local/opt/coreutils/share/man:$MANPATH" &&
     export INFOPATH="/usr/local/opt/coreutils/share/info:$INFOPATH"
 
+# Add texbin to path
+export PATH="/Library/TeX/texbin:$PATH"
+
 # Add node tools to path
 export PATH="$HOME/.local/share/node/bin:$PATH"
 
